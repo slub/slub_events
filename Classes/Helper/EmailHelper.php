de3c30507770949ed38ba8fe82d970897fd6b98f
Alexander Bigga <alexander.bigga@slub-dresden.de>
2015-10-09 17:17:02 +0200

    new fields for statistics

--- Classes/Helper/EmailHelper.php
+++ Classes/Helper/EmailHelper.php
@@ -116,41 +116,6 @@ class EmailHelper {
 	}
 
 	/**
-	 * Function foldline folds the line after 73 signs
-	 * rfc2445.txt: lines SHOULD NOT be longer than 75 octets
-	 *
-	 * @param string		$content: Anystring
-	 * @return string		$content: Manipulated string
-	 */
-	private function foldline($content) {
-
-		$text = trim(strip_tags( html_entity_decode($content), '<br>,<p>,<li>'));
-		$text = preg_replace('/<p[\ \w\=\"]{0,}>/', '', $text);
-		$text = preg_replace('/<li[\ \w\=\"]{0,}>/', '- ', $text);
-		// make newline formated (yes, really write \n into the text!
-		$text = str_replace('</p>', '\n', $text);
-		$text = str_replace('</li>', '\n', $text);
-		// remove tabs
-		$text = str_replace("\t", ' ', $text);
-		// remove multiple spaces
-		$text = preg_replace('/[\ ]{2,}/', '', $text);
-		$text = str_replace('<br />', '\n', $text);
-		// remove more than one empty line
-		$text = preg_replace('/[\n]{3,}/', '\n\n', $text);
-		// remove windows linkebreak
-		$text = preg_replace('/[\r]/', '', $text);
-		// newlines are not allowed
-		$text = str_replace("\n", '\n', $text);
-		// semicolumns are not allowed
-		$text = str_replace(';', '\;', $text);
-
-		$firstline = substr($text, 0, (75-12));
-		$restofline = implode("\n ", str_split(trim(substr($text, (75-12), strlen($text))), 73) );
-
-		return $firstline . "\n ". $restofline;
-	}
-
-	/**
 	 * html2rest
 	 *
 	 * this converts the HTML email to something Rest-Style like text form
