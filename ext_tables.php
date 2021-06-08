57b715d12dd4c6b57c9d9c44efc219185f764020
Thomas Scholze <thomas.scholze@gmail.com>
2021-06-08 10:01:45 +0200

    [TASK] update SLUB Logo / Extension icon

--- ext_tables.php
+++ ext_tables.php
@@ -22,7 +22,7 @@ if (TYPO3_MODE === 'BE') {
         ],
         [
             'access' => 'user,group',
-            'icon'   => 'EXT:slub_events/ext_icon.gif',
+            'icon'   => 'EXT:slub_events/Resources/Public/Icons/Extension.svg',
             'labels' => 'LLL:EXT:slub_events/Resources/Private/Language/locallang_slubevents.xlf',
         ]
     );
