fb2e8c6cfcc052af78a35d20cb08ee80f9d998bf
chrode <christof.rodejohann@slub-dresden.de>
2024-08-16 14:52:00 +0200

    Update ext_emconf.php to new version

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,11 +3,6 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "slub_events".
  *
- * Auto generated 15-09-2014 11:01
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = [
@@ -22,10 +17,10 @@ This extension is developped and used in production at the Saxony State and Univ
     'state'            => 'stable',
     'uploadfolder'     => true,
     'createDirs'       => 'typo3temp/tx_slubevents/',
-    'version'          => '5.1.3',
+    'version'          => '6.0.0',
     'constraints'      => [
         'depends'   => [
-            'typo3'   => '9.5.31-11.5.99',
+            'typo3'   => '11.5.0-11.5.99',
         ],
         'conflicts' => [
         ],
