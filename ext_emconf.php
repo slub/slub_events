e7baa91d7cf1ebcbbba21084e22b64c5839aa57a
chrode <christof.rodejohann@slub-dresden.de>
2025-03-19 10:22:11 +0100

    Raise extension version

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,13 +11,13 @@ $EM_CONF[$_EXTKEY] = [
 
 This extension is developped and used in production at the Saxony State and University Library (SLUB) Dresden, Germany.',
     'category'         => 'plugin',
-    'author'           => 'Alexander Bigga',
+    'author'           => 'SLUB TYPO3 Team',
     'author_email'     => 'typo3@slub-dresden.de',
     'author_company'   => 'SLUB Dresden',
     'state'            => 'stable',
     'uploadfolder'     => true,
     'createDirs'       => 'typo3temp/tx_slubevents/',
-    'version'          => '6.0.0',
+    'version'          => '6.0.1',
     'constraints'      => [
         'depends'   => [
             'typo3'   => '11.5.0-11.5.99',
